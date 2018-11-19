77a8e507b4f9d21f8358ddd9bfe04f4aa30de3c7
qust <13214123320@163.com>
2018-11-19 20:32:14 +0800

    *注释添加

--- src/filter/filter.js
+++ src/filter/filter.js
@@ -1,6 +1,26 @@
+/*
+ * 公共过滤器: filter.js
+ * 参考技术文档: https://cn.vuejs.org/v2/guide/filters.html
+ * <!-- 在双花括号中 -->
+ * {{ message | capitalize }}
+ * <!-- 在 `v-bind` 中 -->
+ * <div v-bind:id="rawId | formatId"></div>
+ */
+
+/**
+ * 过滤器: 值递增1
+ * @param value
+ * @returns {*}
+ */
 const currency = function (value) {
   return value + 1;
 };
+
+/**
+ * 过滤器: 获取文件类型(后缀)
+ * @param fileName
+ * @returns {*}
+ */
 const getFileType = function (fileName) {
   if (fileName) {
     let arr = fileName.split('.');
--- src/main.js
+++ src/main.js
@@ -20,14 +20,22 @@ import Http from './http/comApiHttp';
 Object.keys(Direcitve).forEach(key => {
   Vue.directive(key, Direcitve[key]);
 });
+
 // 自定义过滤器 全局使用
 Object.keys(Filter).forEach(key => {
   Vue.filter(key, Filter[key]);
 });
 
+// 公共工具注册
 Vue.prototype.$util = Util;
+
+// http请求注册
 Vue.prototype.$http = Http;
+
+// 国际化设置为中文
 Vue.use(ElementUI, {locale});
+
+// 设置为false,以阻止vue在启动时生成生产提示
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
