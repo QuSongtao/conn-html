60afca6e3ea1c378bf0507a563b79ca516d5202a
qust <qust@suncd.com>
2019-10-13 22:41:17 +0800

    1、增加丢失日志恢复功能

--- src/views/system/socketLog.vue
+++ src/views/system/socketLog.vue
@@ -5,28 +5,29 @@
         <el-button type="primary" @click="getSendLog" size="small">发送日志</el-button>
         <el-button type="primary" @click="getRecvLog" size="small">接收日志</el-button>
         <el-button type="primary" @click="getSysLog" size="small">系统日志</el-button>
+        <el-button type="primary" @click="getLoserLog" size="small">丢失日志</el-button>
       </el-button-group>
     </el-form>
     <el-row>
-      <el-col :span="6">
+      <el-col :span="7">
         <el-table :data="gridData"
                   :height="tableHeight"
                   :style="{'width': '100%','height': tableHeight}"
                   highlight-current-row
                   @row-dblclick="rowDbClick"
                   ref="dataTable"
-                  border
-        >
+                  border>
           <el-table-column prop="fileName" label="文件名称" width="180" show-overflow-tooltip></el-table-column>
           <el-table-column prop="fileSize" label="大小" width="70"></el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
               <el-button size="mini" @click="download(scope.$index, scope.row)">下载</el-button>
+              <el-button v-if="loser" size="mini" @click="recover(scope.$index, scope.row)">恢复</el-button>
             </template>
           </el-table-column>
         </el-table>
       </el-col>
-      <el-col :span="18" :style="{'height': tableHeight + 'px'}" class="logTextArea">
+      <el-col :span="17" :style="{'height': tableHeight + 'px'}" class="logTextArea">
         <div style="border: 1px solid #e5e5e5;">
           <textarea v-model="logText" class="logarea" :style="{'height': taH + 'px'}"></textarea>
         </div>
@@ -44,20 +45,29 @@ export default {
       tableHeight: document.body.clientHeight - 110,
       taH: document.body.clientHeight - 112,
       pathIndex: '',
-      gridData: []
+      gridData: [],
+      loser: false
     };
   },
   methods: {
     getSendLog: function () {
       this.pathIndex = 'SKS';
+      this.loser = false;
       this.getLogFiles();
     },
     getRecvLog: function () {
       this.pathIndex = 'SKR';
+      this.loser = false;
       this.getLogFiles();
     },
     getSysLog: function () {
       this.pathIndex = 'SKO';
+      this.loser = false;
+      this.getLogFiles();
+    },
+    getLoserLog: function () {
+      this.pathIndex = 'SKL';
+      this.loser = true;
       this.getLogFiles();
     },
     download: function (index, row) {
@@ -78,6 +88,31 @@ export default {
           document.body.appendChild(link);
           link.click();
         }
+      });
+    },
+    recover: function (index, row) {
+      const that = this;
+      this.$confirm('确定恢复该文件?').then(_ => {
+        this.$http.openApiAxios({
+          url: '/mgr/log/recover',
+          method: 'POST',
+          params: {
+            pathIndex: this.pathIndex,
+            fileName: row.fileName
+          },
+          success: function (res) {
+            if (res.meta.code === 1) {
+              that.$message({
+                duration: 0,
+                showClose: true,
+                message: res.data,
+                type: 'warning'
+              });
+            } else {
+              that.$message.error('恢复失败,请查看manager系统日志!');
+            }
+          }
+        });
       });
     },
     rowDbClick: function (row, event, column) {
